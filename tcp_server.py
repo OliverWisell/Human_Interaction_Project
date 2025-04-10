85bb88bce13cb8276da4a21ab0668dce09dd118a
tomroh221 <tom.rosdahl@gmail.com>
2025-04-10 13:26:42 +0200

    felsökning

--- tcp_server.py
+++ tcp_server.py
@@ -1,20 +1,14 @@
 import socket
 
 HOST = '0.0.0.0'
-PORT = 50000
+PORT = 50001
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     server_socket.bind((HOST, PORT))
     server_socket.listen()
     print(f"Listening on {HOST}:{PORT}")
     # Wait for a connection
-    server_socket.settimeout(10)  # Set timeout to 10 seconds
-    try:
-        server_socket.accept()
-    except socket.timeout:
-        print("No connection within the timeout period.")
-        exit(1)
-    # Accept a connection
+   
     conn, addr = server_socket.accept()
     with conn:
         print('Connected by', addr)
