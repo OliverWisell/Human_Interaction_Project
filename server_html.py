0f27c00adc778e611ebacda7d762ac85c327ded0
Drundgren <117913652+Drundgren@users.noreply.github.com>
2025-04-14 11:07:42 +0200

    Fixade lite lokal implementation
    Knappar som ger feedback och håller räkning på hur många gånger de trycks på.

--- server_html.py
+++ server_html.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# Global counters
+yes_count = 0
+no_count = 0
+
 @app.route('/')
+@app.route('/start')
+def start_screen():
+    return render_template('start_screen.html', yes=yes_count, no=no_count)
+
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/choice', methods=['POST'])
+def choice():
+    global yes_count, no_count
+    data = request.get_json()
+    if data['choice'] == 'yes':
+        yes_count += 1
+    elif data['choice'] == 'no':
+        no_count += 1
+    return '', 204
 
 @socketio.on('join')
 def handle_join(data):
@@ -14,4 +33,4 @@ def handle_join(data):
     emit('welcome', {'msg': f"Hej {data['name']}!"}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=50001, debug=True)
+    socketio.run(app, debug=True)
