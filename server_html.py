879da2a4b947e59b5703dc59e08185307a68a25c
Drundgren <117913652+Drundgren@users.noreply.github.com>
2025-04-14 11:20:29 +0200

    la till en enkel admin

--- server_html.py
+++ server_html.py
@@ -4,18 +4,23 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Global counters
 yes_count = 0
 no_count = 0
+connected_users = 0
+admin_sid = None  # Store the admin socket ID
 
 @app.route('/')
 @app.route('/start')
 def start_screen():
-    return render_template('start_screen.html', yes=yes_count, no=no_count)
+    return render_template('start_screen.html')
 
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 @app.route('/choice', methods=['POST'])
 def choice():
@@ -25,7 +30,33 @@ def choice():
         yes_count += 1
     elif data['choice'] == 'no':
         no_count += 1
+    send_counts()
     return '', 204
+
+@socketio.on('connect')
+def on_connect():
+    global connected_users
+    connected_users += 1
+    send_counts()
+
+@socketio.on('disconnect')
+def on_disconnect():
+    global connected_users
+    connected_users = max(0, connected_users - 1)
+    send_counts()
+
+@socketio.on('admin_view')
+def register_admin():
+    global admin_sid
+    admin_sid = request.sid
+    send_counts()
+
+def send_counts():
+    socketio.emit('counts', {
+        'yes': yes_count,
+        'no': no_count,
+        'users': connected_users
+    })
 
 @socketio.on('join')
 def handle_join(data):
