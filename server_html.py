c16a7ff3d17a5513a63e8ffea8b177fc4589b51c
tomroh221 <tom.rosdahl@gmail.com>
2025-04-14 13:09:08 +0200

    Implement of Admin and client

--- server_html.py
+++ server_html.py
@@ -1,17 +1,46 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room
 
+
+# Flask app with SocketIO for real-time communication
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+clients = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
 @socketio.on('join')
 def handle_join(data):
-    print(f"{data['name']} joined")
-    emit('welcome', {'msg': f"Hej {data['name']}!"}, broadcast=True)
+    name = data.get('name')
+    clients[request.sid] = name
+    print(f"{name } joined with session ID {request.sid}")
+    emit('player_joined', {'name': name }, broadcast=True)
+
+
+@socketio.on('start')
+def handle_start():
+    print("Game started")
+    emit('start_question', broadcast=True)
+
+@socketio.on('answer')
+def handle_answer(data):
+    name = clients.get(request.sid, 'Unknown')
+    choice = data.get('choice')
+    print(f"{name} answered: {choice}")
+   
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    name = clients.pop(request.sid, 'Okänd')
+    print(f"{name} disconnected")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=50001, debug=True)
+    #socketio.run(app, host='0.0.0.0', port=50001, debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
